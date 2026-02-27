fae40a64f511037e2eeda46f4ce0115185b85966
Han Sun <hanicesun>
2026-02-27 12:26:20 -0800

    hs

--- src/packBase/cli.py
+++ src/packBase/cli.py
@@ -8,7 +8,7 @@ def get_parser():
     parser.add_argument('--package', type=str, default=None, help='package name, using the current directory name if not specified')
     parser.add_argument('--author', type=str, default='Hanice Sun', help='author name')
     parser.add_argument('--email', type=str, default='hanicesun@gmail.com', help='email address')
-    parser.add_argument('--github_account', type=str, default='HaniceSun', help='github account')
+    parser.add_argument('--account', type=str, default='HaniceSun', help='github account')
     return parser
 
 def main():
@@ -17,5 +17,5 @@ def main():
     package_name = args.package
     if package_name is None:
         package_name = os.path.basename(os.getcwd())
-    ps = PackSetup(package_name=package_name, author=args.author, email=args.email, github_account=args.github_account)
+    ps = PackSetup(package_name=package_name, author=args.author, email=args.email, github_account=args.account)
     ps()
