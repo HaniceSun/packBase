422071a2f451f933b97c0b55e6bf721af3c9ea21
Han Sun <hanicesun>
2026-02-26 19:45:08 -0800

    hs

--- src/packBase/cli.py
+++ src/packBase/cli.py
@@ -8,8 +8,8 @@ def get_parser():
     parser.add_argument('--package', type=str, default=None, help='package name, using the current directory name if not specified')
     parser.add_argument('--author', type=str, default='Hanice Sun', help='author name')
     parser.add_argument('--email', type=str, default='hanicesun@gmail.com', help='email address')
-    parser.add_argument('--gtihub_https', type=str, default='https://github.com/HaniceSun', help='https url of your github')
-    parser.add_argument('--gtihub_ssh', type=str, default='git@github.com:HaniceSun', help='ssh url of your github')
+    parser.add_argument('--github_https', type=str, default='https://github.com/HaniceSun', help='https url of your github')
+    parser.add_argument('--github_ssh', type=str, default='git@github.com:HaniceSun', help='ssh url of your github')
     return parser
 
 def main():
@@ -18,5 +18,5 @@ def main():
     package_name = args.package
     if package_name is None:
         package_name = os.path.basename(os.getcwd())
-    ps = PackSetup(package_name=package_name, author=args.author, email=args.email, gtihub_https=args.gtihub_https, gtihub_ssh=args.gtihub_ssh)
+    ps = PackSetup(package_name=package_name, author=args.author, email=args.email, github_https=args.github_https, github_ssh=args.github_ssh)
     ps()
